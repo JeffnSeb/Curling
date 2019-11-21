cc3dffa1f30391b504d13c0c657d3edc7c756114
Jeff Rudd <jeffrudd19@gmail.com>
2019-11-21 12:35:23 -0500

    Added all the id's to the grid

--- scripts.js
+++ scripts.js
@@ -1,39 +1,77 @@
-let drawSubmit = document.getElementById('drawButton')
+let lightSubmit = document.getElementById('lightButton')
+let mediumSubmit = document.getElementById('mediumButton')
+let heavySubmit = document.getElementById('heavyButton')
 let hitSubmit = document.getElementById('hitButton')
+
 let tossSubmit = document.getElementById('tossButton')
-let x1Line = document.getElementById('x1-y1')
-let x2Line = document.getElementById('x2-y1')
-let x3Line = document.getElementById('x3-y1')
-let x4Line = document.getElementById('x4-y1')
-let x5Line = document.getElementById('x5-y1')
+
+let x1y1 = document.getElementById('x1-y1')
+let x2y1 = document.getElementById('x2-y1')
+let x3y1 = document.getElementById('x3-y1')
+let x4y1 = document.getElementById('x4-y1')
+let x5y1 = document.getElementById('x5-y1')
+let x1y2 = document.getElementById('x1-y2')
+let x2y2 = document.getElementById('x2-y2')
+let x3y2 = document.getElementById('x3-y2')
+let x4y2 = document.getElementById('x4-y2')
+let x5y2 = document.getElementById('x5-y2')
+let x1y3 = document.getElementById('x1-y3')
+let x2y3 = document.getElementById('x2-y3')
+let x3y3 = document.getElementById('x3-y3')
+let x4y3 = document.getElementById('x4-y3')
+let x5y3 = document.getElementById('x5-y3')
+let x1y4 = document.getElementById('x1-y4')
+let x2y4 = document.getElementById('x2-y4')
+let x3y4 = document.getElementById('x3-y4')
+let x4y4 = document.getElementById('x4-y4')
+let x5y4 = document.getElementById('x5-y4')
+let x1y6 = document.getElementById('x1-y6')
+let x2y6 = document.getElementById('x2-y6')
+let x3y6 = document.getElementById('x3-y6')
+let x4y6 = document.getElementById('x4-y6')
+let x5y6 = document.getElementById('x5-y6')
+let x1y7 = document.getElementById('x1-y7')
+let x2y7 = document.getElementById('x2-y7')
+let x3y7 = document.getElementById('x3-y7')
+let x4y7 = document.getElementById('x4-y7')
+let x5y7 = document.getElementById('x5-y7')
+
+
+
 
 let lineChoice = ''
-x1Line.addEventListener('click', function(){
-    console.log('Line1')
+x1y1.addEventListener('click', function(){
     lineChoice = '1'
 })
-x2Line.addEventListener('click', function(){
-    console.log('Line2')
+x2y1.addEventListener('click', function(){
     lineChoice = '2'
 })
-x3Line.addEventListener('click', function(){
-    console.log('Line3')
+x3y1.addEventListener('click', function(){
     lineChoice = '3'
 })
-x4Line.addEventListener('click', function(){
-    console.log('Line4')
+x4y1.addEventListener('click', function(){
     lineChoice = '4'
 })
-x5Line.addEventListener('click', function(){
-    console.log('Line5')
+x5y1.addEventListener('click', function(){
     lineChoice = '5'
 })
 
 let speedChoice = ''
-drawSubmit.addEventListener('click', function(event){
+lightSubmit.addEventListener('click', function(event){
     event.preventDefault()
-    speedChoice = 'draw'
+    speedChoice = 'light'
 })
+
+mediumSubmit.addEventListener('click', function(event){
+    event.preventDefault()
+    speedChoice = 'medium'
+})
+
+heavySubmit.addEventListener('click', function(event){
+    event.preventDefault()
+    speedChoice = 'heavy'
+})
+
 
 hitSubmit.addEventListener('click', function(event){
     event.preventDefault()
@@ -47,21 +85,21 @@ tossSubmit.addEventListener('click', function(event){
 
 //write a function that takes the speed picked and line picked, then outputs the new rock location
 const tossRock = function () {
-    if (lineChoice === '1' && speedChoice ==='draw') {
-        x1Line.style.background = "blue"
-        console.log('1')
-    } else if (lineChoice === '2' && speedChoice ==='draw') {
+    if (lineChoice === '1' && speedChoice ==='heavy') {
+        if (Math.random() > 0.5) {
+            x1y1.style.background = "blue"
+        } else {
+            x1y2.style.background = "blue"
+        }
+        
+    } else if (lineChoice === '2' && speedChoice ==='heavy') {
         x2Line.style.background = "blue"
-        console.log('2')
-    } else if (lineChoice === '3' && speedChoice ==='draw') {
+    } else if (lineChoice === '3' && speedChoice ==='heavy') {
         x3Line.style.background = "blue"
-        console.log('3')
-    } else if (lineChoice === '4' && speedChoice ==='draw') {
+    } else if (lineChoice === '4' && speedChoice ==='heavy') {
         x4Line.style.background = "blue"
-        console.log('4')
-    } else if (lineChoice === '5' && speedChoice ==='draw') {
+    } else if (lineChoice === '5' && speedChoice ==='heavy') {
         x5Line.style.background = "blue"
-        console.log('5')
 
     } else if (lineChoice === '1' && speedChoice ==='hit') {
         if(x1Line.style.background === "blue") {
